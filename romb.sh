b60a2a3546926fe4b7fe097e9e1b3a07ae679c60
danxxo <90875721+danxxo@users.noreply.github.com>
2022-10-26 00:29:20 +0300

    Update romb.sh

--- romb.sh
+++ romb.sh
@@ -1,55 +1,49 @@
 #!/bin/bash
 
-#проверяем есть ли входные аргументы
-if [ $# -eq 0 ];
-then 
+# checking programm arguments
+if [ $# -eq 0 ]; then 
 echo "Amnesia..need arguments"
 exit 1
 fi
 
-# проверка четности входного аргумента
-odd_or_honest=$(($1 % 2))
-
-if(($odd_or_honest))
-then
-dot_counter=1
-else
-dot_counter=2
+# zero value handling
+if (( $1 == 0 )); then
+exit 0
 fi
 
-#рисование
-space_counter=$(($1 / 2))
+# half rhombus painting
+space_counter=$(($1 - 1))
+dot_counter=1
 
-for((i = 0; i < $1; i++))
-do
-	for((i = 0; i < $space_counter; i++))
-	do
-	echo -n " "
+for((i = 0; i <= $1; i++)); do
+	for((i = 0; i < $space_counter; i++)); do
+	echo -n "  "
 	done
 
-		for((i = 0; i < $dot_counter; i++))
-		do
-		echo -n "."
-		done
+	for((i = 0; i < $dot_counter; i++)); do
+	echo -n " .  "
+	done
 echo
 let "space_counter-=1"
-let "dot_counter+=2"
+let "dot_counter+=1"
 done
 
+# half of rhombus's already painted
+# updating values
 let "space_counter+=2"
-let "dot_counter-=4"
+let "dot_counter-=2"
 
-for((j = $1; j > 0; j--))
-do
-	for((j = 0; j < $space_counter; j++))
-	do
-	echo -n " "
+# painting second part of rhombus
+for((j = $1; j > 0; j--)); do
+	for((j = 0; j < $space_counter; j++)); do
+	echo -n "  "
 	done
-		for((j = 0; j < $dot_counter; j++))
-		do
-		echo -n "."
-		done
+	
+	for((j = 0; j < $dot_counter; j++)); do
+	echo -n " .  "
+	done
 echo
 let "space_counter+=1"
-let "dot_counter-=2"
+let "dot_counter-=1"
 done
+exit 0

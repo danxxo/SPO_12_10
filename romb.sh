723010719aeb2c325c189c43ee9b5d6ae6560470
danxxo <90875721+danxxo@users.noreply.github.com>
2022-10-26 00:39:29 +0300

    Update romb.sh

--- romb.sh
+++ romb.sh
@@ -2,13 +2,13 @@
 
 # checking programm arguments
 if [ $# -eq 0 ]; then 
-echo "Amnesia..need arguments"
-exit 1
+	echo "Amnesia..need arguments"
+	exit 1
 fi
 
 # zero value handling
 if (( $1 == 0 )); then
-exit 0
+	exit 0
 fi
 
 # half rhombus painting
@@ -17,11 +17,11 @@ dot_counter=1
 
 for((i = 0; i <= $1; i++)); do
 	for((i = 0; i < $space_counter; i++)); do
-	echo -n "  "
+		echo -n "  "
 	done
 
 	for((i = 0; i < $dot_counter; i++)); do
-	echo -n " .  "
+		echo -n " .  "
 	done
 echo
 let "space_counter-=1"
@@ -36,11 +36,11 @@ let "dot_counter-=2"
 # painting second part of rhombus
 for((j = $1; j > 0; j--)); do
 	for((j = 0; j < $space_counter; j++)); do
-	echo -n "  "
+		echo -n "  "
 	done
 	
 	for((j = 0; j < $dot_counter; j++)); do
-	echo -n " .  "
+		echo -n " .  "
 	done
 echo
 let "space_counter+=1"

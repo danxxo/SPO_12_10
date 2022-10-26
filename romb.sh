965443181f7d52c88cbd871b86e3a34fec9b780b
danxxo <90875721+danxxo@users.noreply.github.com>
2022-10-26 18:54:03 +0300

    Update romb.sh

--- romb.sh
+++ romb.sh
@@ -15,6 +15,12 @@ fi
 space_counter=$(($1 - 1))
 dot_counter=1
 
+# iterator j in 1st cycle goes from 0 to $1, need for painting $1 lines
+# iterator j in 2nd cycle goes from 0 to space_counter - 1, need for tabulation
+# iterator j in 3d cycle goes from 0 to dot_counter - 1, need for dot painting
+# value space_counter need for tabulation, it goes from $1 - 1 to 0
+# value dot_counter need for amount of dots in line, it goes from 1 to $1
+
 for((i = 0; i <= $1; i++)); do
 	for((i = 0; i < $space_counter; i++)); do
 		echo -n "  "
@@ -34,6 +40,12 @@ let "space_counter+=2"
 let "dot_counter-=2"
 
 # painting second part of rhombus
+# iterator j in 1st cycle goes from 0 to $1 - 1, need for painting lines wthout line with $1 dots
+# iterator j in 1st cycle goes from $1 to 1, need for tabulation
+# iterator j in 2nd cycle goes from 0 to space_counter - 1, need for dot painting
+# value space_counter need for tabulation, it goes from 1 to $1
+# value dot_counter need for amount of dots in line, it goes from $1 - 1 to 1
+
 for((j = $1; j > 0; j--)); do
 	for((j = 0; j < $space_counter; j++)); do
 		echo -n "  "

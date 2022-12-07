552d6a9709240dccc6d3aa02440ed5efeeb64946
danxxo <90875721+danxxo@users.noreply.github.com>
2022-12-07 14:17:11 +0300

    Update task3_2.sh

--- Task3/task3_2.sh
+++ Task3/task3_2.sh
@@ -1,56 +1,86 @@
-declare if_int='^[0-9]+$'
+#!/bin/bash
 
+# function for replacing elements inside massive
+# takes two input parametres and swap it
 
 swap() {
-  tmp=${phrase[$1]}
-  phrase[$1]=${phrase[$2]}
-  phrase[$2]=$tmp
+	tmp=${phrase[$1]}
+	phrase[$1]=${phrase[$2]}
+	phrase[$2]=$tmp
 }
 
+# function that takes the string, read every element
+# if element is a char with down_register, it change it to up_register and add to string 
+# using tr command utility, can see tr --help
+
 up_register() {
-string=$@
-echo ----------------------------------------
-echo input string : $string
-for ((i=0; i<${#string}; i++)); do
-	char=${string:$i:1}
-	char_ascii=$(echo -n $char | od -An -tuC)
-	 
-	if [[ $char_ascii -gt 96 ]] && [[ $char_ascii -lt 123 ]]; then
-		char_ascii=$(( $char_ascii - 32 ))
-		char_up=$(printf "\x$(printf %x $char_ascii)")
-		string=$(echo $string | tr $char $char_up )
-		echo old char : $char , new char : $char_up
-	fi	
-done
+	string=$@
+
+	for ((i=0; i<${#string}; i++)); do
+		char=${string:$i:1}
+		char_ascii=$(echo -n $char | od -An -tuC)
+		 
+		if [[ $char_ascii -gt 96 ]] && [[ $char_ascii -lt 123 ]]; then
+			char_ascii=$(( $char_ascii - 32 ))
+			char_up=$(printf "\x$(printf %x $char_ascii)")
+			string=$(echo $string | tr $char $char_up )
+		fi	
+	done
 }
+
+# function that takes the string, read every element
+# if element is a char with up_register, it change it down_register and add to string 
+# using tr command utility, can see tr --help
+
+down_register() {
+	string=$@
+
+	for ((i=0; i<${#string}; i++)); do
+		char=${string:$i:1}
+		char_ascii=$(echo -n $char | od -An -tuC)
+		 
+		if [[ $char_ascii -gt 64 ]] && [[ $char_ascii -lt 91 ]]; then
+			char_ascii=$(( $char_ascii + 32 ))
+			char_up=$(printf "\x$(printf %x $char_ascii)")
+			string=$(echo $string | tr $char $char_up )
+		fi	
+	done
+}
+
+# reading phrase -p to write before reading
+# -r need to turn off escape symbols
+# -a read to array
 
 read -p "Please write your phrase: " -r -a phrase
 
-for (( i = 0; i < ${#phrase[@]}; i++ )); do
-  #если элемент нечетный
-  if ((i % 2)); then
-    if (( ${#phrase[$i]} < ${#phrase[$(($i + 1))]} )); then
-      #если не выдолняется условие P, то меняем местами текущий и следующий
-      swap $i $(($i + 1))
-    fi
-
-  else
-    if (( ${#phrase[$i]} > ${#phrase[$(($i + 1))]} )); then
-      #если не выдолняется условие P, то меняем местами текущий и следующий
-      swap $i $(($i + 1))
-    fi
-
-  fi
-
+# sorting massive "phrase"
+# we need to make phrase[0] <= phrase[1] >= phrase[2] <= ...
+# if index is odd we check if phr[i] > phr[i+1], then swipe i and i+1
+# if index is non-odd we check phr[i] < phr[i+1], then swipe them
+for (( i = 0; i < ${#phrase[@]}-1; i++ )); do
+	if ((i % 2)); then
+		if (( ${#phrase[$i]} < ${#phrase[$(($i + 1))]} )); then
+			swap $i $(($i + 1))
+		fi
+	else
+		if (( ${#phrase[$i]} > ${#phrase[$(($i + 1))]} )); then
+			swap $i $(($i + 1))
+		fi
+	fi
 done
 
-for i in ${phrase[@]}; do
-  echo $i
+# writing sorted massive and up or down register of elements
+for ((j=0; j<${#phrase[@]}; j++)); do
+	if (($j % 2)); then 
+		string=${phrase[$j]}
+		up_register $string
+		echo -n $string" "
+	else 
+		string=${phrase[$j]}
+		down_register $string
+		echo -n $string" "
+
+	fi
 done
-
-
-string="haclhLICDBIBUVDdvbidbkkdsbLIDVBIB"
-up_register $string
-
-echo $string
+echo
 exit 0
